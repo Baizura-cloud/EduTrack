3c0afd089be29bf06aa35f7ca132872ff0208599
baizura <baizura1996@gmail.com>
2024-09-11 14:20:20 +0800

    redux todo component

--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -1,20 +1,49 @@
-import React, { useState, useEffect  } from "react";
-import { useDispatch, useSelector  } from "react-redux";
-import { addTodo } from "../redux/todoSlice";
-import { Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo,  toggleComplete } from "../redux/todoSlice";
+import { Button, Typography } from "@mui/material";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
 
 const AddTodoForm = () => {
   const [value, setValue] = useState("");
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+  const [checked, setChecked] = React.useState([0]);
 
-  
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+
+    dispatch(toggleComplete({id: value.id, completed: !value.completed}))
+  };
+
+  const handleDelete = (todo) =>{
+    console.log(todo)
+    dispatch(deleteTodo({id:todo.id}))
+  }
+
   const onSubmit = (event) => {
     event.preventDefault();
     if (value) {
       dispatch(
         addTodo({
-        id: 1,
+          id: 1,
           title: value,
         })
       );
@@ -37,13 +66,37 @@ const AddTodoForm = () => {
           Submit
         </button>
       </form>
-      <ul className='list-group'>
-            {todos.map((todo) => (
-                <Typography>
-                    {todo.id} {todo.title} {todo.completed}
-                </Typography> 
-            ))}
-        </ul>
+       <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {todos.map((todo) => {
+        const labelId = `checkbox-list-label-${todo.id}`;
+
+        return (
+          <ListItem
+            key={todo.id}
+            secondaryAction={
+              <IconButton edge="end" aria-label="comments">
+                <CommentIcon />
+              </IconButton>
+            }
+            disablePadding
+          >
+            <ListItemButton role={undefined} onClick={handleToggle(todo)} dense>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={todo.completed}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={todo.title} />
+              
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
     </div>
   );
 };
